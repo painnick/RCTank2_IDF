77e6bc220f611560a6c595eda6e743af07c257d9
painnick <painnick@gmail.com>
2026-01-31 14:01:28 +0900

    REINIT.

--- main/main.c
+++ main/main.c
@@ -1,51 +1,48 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2019 Ricardo Quesada
-// http://retro.moe/unijoysticle2
+// Example file - Public Domain
+// Need help? http://bit.ly/bluepad32-help
 
-#include "sdkconfig.h"
+#include <stdlib.h>
 
-#include <stddef.h>
-
-// BTstack related
 #include <btstack_port_esp32.h>
 #include <btstack_run_loop.h>
 #include <btstack_stdio_esp32.h>
-
-// Bluepad32 related
-#include <arduino_platform.h>
+#include <hci_dump.h>
+#include <hci_dump_embedded_stdout.h>
 #include <uni.h>
 
-//
-// Autostart
-//
-#if CONFIG_AUTOSTART_ARDUINO
-void initBluepad32() {
-#else
-int app_main(void) {
-#endif  // !CONFIG_AUTOSTART_ARDUINO
-    // hci_dump_open(NULL, HCI_DUMP_STDOUT);
 
-// Don't use BTstack buffered UART. It conflicts with the console.
-#ifndef CONFIG_ESP_CONSOLE_UART_NONE
+#include "sdkconfig.h"
+
+// Sanity check
+#ifndef CONFIG_BLUEPAD32_PLATFORM_CUSTOM
+#error "Must use BLUEPAD32_PLATFORM_CUSTOM"
+#endif
+
+// Defined in my_platform.c
+struct uni_platform* get_my_platform(void);
+
+int app_main(void) {
+    // If you enable HCI Dump better to disable "Bluepad32 USB Console" from "idf.py menuconfig".
+    // hci_dump_init(hci_dump_embedded_stdout_get_instance());
+
+    // Don't use BTstack buffered UART. It conflicts with the console.
+#ifdef CONFIG_ESP_CONSOLE_UART
 #ifndef CONFIG_BLUEPAD32_USB_CONSOLE_ENABLE
     btstack_stdio_init();
 #endif  // CONFIG_BLUEPAD32_USB_CONSOLE_ENABLE
-#endif  // CONFIG_ESP_CONSOLE_UART_NONE
+#endif  // CONFIG_ESP_CONSOLE_UART
 
     // Configure BTstack for ESP32 VHCI Controller
     btstack_init();
 
-    // hci_dump_init(hci_dump_embedded_stdout_get_instance());
-
     // Must be called before uni_init()
-    uni_platform_set_custom(get_arduino_platform());
+    uni_platform_set_custom(get_my_platform());
 
     // Init Bluepad32.
     uni_init(0 /* argc */, NULL /* argv */);
 
     // Does not return.
     btstack_run_loop_execute();
-#if !CONFIG_AUTOSTART_ARDUINO
+
     return 0;
-#endif  // !CONFIG_AUTOSTART_ARDUINO
 }
